0840c8338c29417eb31622c4a4ba1a43920b2483
saman <saman.esmaili204@gmail.com>
2024-08-29 13:47:14 +0330

    connect to database

--- web_site/users/views.py
+++ web_site/users/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+
+from users.models import Users, Post
+
+
 def index(request):
     return render(request, "users/index.html")
 
@@ -14,5 +18,10 @@ def calendar(request):
     return render(request,"users/calendar.html")
 
 def contact(request):
+    if request.method == 'POST':
+        user = Users(full_name=request.POST['txtName'],email=request.POST['txtEmail'])
+        user.save()
+        comment = Post(comment=request.POST['txtComment'], users_id=user.id)
+        comment.save()
     return render(request,"users/contact.html")
 
--- web_site/web_site/settings.py
+++ web_site/web_site/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'web_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'website',
+        'USER': 'root',
+        'PASSWORD': 'saman123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
