3f93e233a66e3d8c468e8e76abb2ddbb472352d9
saman <saman.esmaili204@gmail.com>
2024-09-01 10:42:41 +0330

    fix tabs and web pages

--- web_site/users/views.py
+++ web_site/users/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 
-from users.models import Users, Post
+from users.models import Customer, Post
 
 
 def index(request):
-    return render(request, "users/index.html")
+    return render(request, "users/index.html",{'id':'page1'})
 
 def about(request):
-    return render(request,"users/about.html")
+    return render(request,"users/about.html",{'id':'page2'})
 
 def courses(request):
-    return render(request,"users/courses.html")
+    return render(request,"users/courses.html",{'id':'page3'})
 
 def recipes(request):
-    return render(request,"users/recipes.html")
+    return render(request,"users/recipes.html",{'id':'page4'})
 def calendar(request):
-    return render(request,"users/calendar.html")
+    return render(request,"users/calendar.html",{'id':'page5'})
 
 def contact(request):
     if request.method == 'POST':
-        user = Users(full_name=request.POST['txtName'],email=request.POST['txtEmail'])
+        user = Customer(full_name=request.POST['txtName'],email=request.POST['txtEmail'])
         user.save()
         comment = Post(comment=request.POST['txtComment'], users_id=user.id)
         comment.save()
-    return render(request,"users/contact.html")
+    return render(request,"users/contact.html",{'id':'page6'})
 
