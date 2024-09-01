437679aec1e378f47a4888ceae8762a5601dae68
saman <saman.esmaili204@gmail.com>
2024-09-01 10:42:05 +0330

    change user to customer

--- web_site/users/models.py
+++ web_site/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Users(models.Model):
+class Customer(models.Model):
     full_name = models.CharField(max_length=30,blank=False,null=False)
     email = models.CharField(max_length=50,blank=False,null=False)
 
 class Post(models.Model):
     comment = models.CharField(max_length=250,blank=False,null=False)
-    users = models.ForeignKey(Users,on_delete=models.CASCADE)
+    users = models.ForeignKey(Customer,on_delete=models.CASCADE)
